843c1d130bddcaac930c5c7cc0d0fb9423fa3484
SUGAN-2007 <developer.sugan@gmail.com>
2025-07-30 19:21:27 +0530

    begin

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MovieCard from './Compnents/Moviecard'
+
+function App() {
+  return (
+    <>
+        <MovieCard movie={{title:"John wick",release_date: "2023"}}/>
+    </>
+  )
+}
+
+export default App
