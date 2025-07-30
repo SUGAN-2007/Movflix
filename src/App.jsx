30141245202394a4e5116ed9b8f16ddff617b95f
SUGAN-2007 <developer.sugan@gmail.com>
2025-07-30 19:37:46 +0530

    half

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import MovieCard from './Compnents/Moviecard'
+import MovieCard from './Components/Moviecard'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <>
-        <MovieCard movie={{title:"John wick",release_date: "2023"}}/>
+        <Home/>
     </>
   )
 }
